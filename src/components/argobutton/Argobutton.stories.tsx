028f6666fe84e293e7e6441d32fd0b09f99fa213
Tim <44584257+TimothyNaumov@users.noreply.github.com>
2024-03-18 12:41:31 -0500

    feat: created Argobutton

---
+++ src/components/argobutton/Argobutton.stories.tsx
@@ -0,0 +1,54 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Button from "./Argobutton";
+
+export default {
+  title: "Inputs/Argo Button",
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  variant: "text",
+  children: "Default",
+};
+
+export const Contained = Template.bind({});
+Contained.args = {
+  variant: "contained",
+  children: "Contained",
+};
+
+export const Outlined = Template.bind({});
+Outlined.args = {
+  variant: "outlined",
+  children: "outlined",
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+  children: "disabled",
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  color: "secondary",
+  children: "secondary",
+};
+
+export const Success = Template.bind({});
+Success.args = {
+  color: "success",
+  variant: "contained",
+  children: "success",
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  color: "error",
+  variant: "outlined",
+  children: "error",
+};
