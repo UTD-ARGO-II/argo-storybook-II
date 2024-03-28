6ae08c8f5a701a15da51e2848bd3f5ab42d641eb
Alina <121243874+gimmedemnomnoms@users.noreply.github.com>
2024-03-28 10:48:03 -0500

    updated custom theme

--- src/themes/custom.theme.js
+++ src/themes/custom.theme.js
@@ -2,18 +2,19 @@ import { createTheme } from "@mui/material";
 import fireengine from "../colors/fireengine";
 import turquoise from "../colors/turquoise";
 
+
 export const customTheme = createTheme({
   palette: {
     contrastThreshold: 4.5,
     mode: "dark",
     primary: {
-      main: fireengine["200"],
+      main: fireengine["500"],
     },
     secondary: {
-      main: turquoise["300"],
+      main: turquoise["900"],
     },
     background: {
-      default: turquoise["400"],
+      default: turquoise["100"],
       paper: fireengine["100"],
     },
   },
