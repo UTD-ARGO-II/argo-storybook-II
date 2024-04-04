d592383326439e0587c476df26d1b9f226f59ce0
Alina <121243874+gimmedemnomnoms@users.noreply.github.com>
2024-04-04 12:01:01 -0500

    UG3-93 figma integration for Dropdown stories

--- src/components/dropdown/Dropdown.stories.tsx
+++ src/components/dropdown/Dropdown.stories.tsx
@@ -12,6 +12,12 @@ const Template: ComponentStory<typeof Dropdown> = (args) => (
 
 const items = [10, 20, 30];
 export const Primary = Template.bind({});
+Primary.parameters = {
+	design: {
+		type: 'figma',
+		url: 'https://www.figma.com/file/WrbYfhYCrGCb7BUOPSCCRB/ARGO-X-design-library?node-id=1462%3A17979&mode=dev'
+	}
+}
 Primary.args = {
 	label: 'Label',
 	items: items
