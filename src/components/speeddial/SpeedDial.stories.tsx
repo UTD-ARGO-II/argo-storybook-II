8cb3ad8cb181339a5054d83ace8b556ca42c1980
Lillie McMaster <lillie.mcmaster@gmail.com>
2024-04-17 19:06:03 -0500

    fix: speed dial

--- src/components/speeddial/SpeedDial.stories.tsx
+++ src/components/speeddial/SpeedDial.stories.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import BasicSpeedDial from './SpeedDial';
+import SpeedDial from './SpeedDial';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default {
   title: 'Navigation/SpeedDial',
-  component: BasicSpeedDial,
-} as ComponentMeta<typeof BasicSpeedDial>;
+  component: SpeedDial,
+} as ComponentMeta<typeof SpeedDial>;
 
-const Template: ComponentStory<typeof BasicSpeedDial> = () => <BasicSpeedDial />;
+const Template: ComponentStory<typeof SpeedDial> = (args) => <SpeedDial {...args} />;
 
-export const Default = Template.bind({});
+export const Basic = Template.bind({});
+Basic.args = {
+  direction: 'up',
+  actions: [
+    { icon: <AddIcon />, name: 'Add', onClick: () => console.log('Add action') },
+    { icon: <EditIcon />, name: 'Edit', onClick: () => console.log('Edit action') },
+    { icon: <DeleteIcon />, name: 'Delete', onClick: () => console.log('Delete action') }
+  ]
+};
+
+
+export const LeftOpening = Template.bind({});
+LeftOpening.args = {
+  direction: 'left',
+  actions: [
+    { icon: <AddIcon />, name: 'Add', onClick: () => console.log('Add action') },
+    { icon: <EditIcon />, name: 'Edit', onClick: () => console.log('Edit action') },
+    { icon: <DeleteIcon />, name: 'Delete', onClick: () => console.log('Delete action') }
+  ]
+};
--- src/components/speeddial/SpeedDial.tsx
+++ src/components/speeddial/SpeedDial.tsx
@@ -1,37 +1,37 @@
-import Box from '@mui/material/Box';
-import SpeedDial from '@mui/material/SpeedDial';
-import SpeedDialIcon from '@mui/material/SpeedDialIcon';
-import SpeedDialAction from '@mui/material/SpeedDialAction';
-import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import PrintIcon from '@mui/icons-material/Print';
-import ShareIcon from '@mui/icons-material/Share';
+import React from 'react';
+import { SpeedDial as MuiSpeedDial, SpeedDialIcon, SpeedDialAction } from '@mui/material';
 
-const actions = [
-  { icon: <FileCopyIcon />, name: 'Copy' },
-  { icon: <SaveIcon />, name: 'Save' },
-  { icon: <PrintIcon />, name: 'Print' },
-  { icon: <ShareIcon />, name: 'Share' },
-];
+export interface SpeedDialProps {
+  actions: Array<{
+    icon: JSX.Element,
+    name: string,
+    onClick?: () => void
+  }>;
+  direction?: 'up' | 'down' | 'left' | 'right';
+}
 
-const BasicSpeedDial = () => {
+const SpeedDial: React.FC<SpeedDialProps> = ({ actions, direction = 'up' }) => {
   return (
-    <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 }}>
-      <SpeedDial
-        ariaLabel="SpeedDial basic example"
-        sx={{ position: 'absolute', bottom: 16, right: 16 }}
-        icon={<SpeedDialIcon />}
-      >
-        {actions.map((action) => (
-          <SpeedDialAction
-            key={action.name}
-            icon={action.icon}
-            tooltipTitle={action.name}
-          />
-        ))}
-      </SpeedDial>
-    </Box>
+    <MuiSpeedDial
+      ariaLabel="Speed Dial"
+      sx={{
+        position: 'fixed',
+        bottom: 16, // 16 pixels from the bottom
+        right: 16  // 16 pixels from the right
+      }}
+      icon={<SpeedDialIcon />}
+      direction={direction}
+    >
+      {actions.map((action, index) => (
+        <SpeedDialAction
+          key={index}
+          icon={action.icon}
+          tooltipTitle={action.name}
+          onClick={action.onClick}
+        />
+      ))}
+    </MuiSpeedDial>
   );
-};
+}
 
-export default BasicSpeedDial;
+export default SpeedDial;
